687fefe2377c1e38ad88c0b493fa3d53b239277d
Dianamoon18 <72127610+Diamoon18@users.noreply.github.com>
2021-12-19 15:39:05 +0100

    Update negation_cpu.c

--- negation_cpu.c
+++ negation_cpu.c
@@ -5,62 +5,62 @@
 #include<time.h>
 
 void negation(char* filename){
-		FILE* fp = fopen(filename, "rb");
+	FILE* fp = fopen(filename, "rb");
 
-		unsigned char *imageData; 
-		unsigned char *newimageData; 
-  	unsigned char imageHeader[54]; 
-  	unsigned char colorTable[1024];
+	unsigned char *imageData; 
+	unsigned char *newimageData; 
+	unsigned char imageHeader[54]; 
+	unsigned char colorTable[1024];
 
-		int i,j;
+	int i,j;
 
-		fread(imageHeader, sizeof(unsigned char), 54, fp);
-	
-		int width = *(int*)&imageHeader[18];
-		int height = *(int*)&imageHeader[22];
-		int bitDepth = *(int*)&imageHeader[28];
+	fread(imageHeader, sizeof(unsigned char), 54, fp);
 
-		int imgDataSize = width * height; 
+	int width = *(int*)&imageHeader[18];
+	int height = *(int*)&imageHeader[22];
+	int bitDepth = *(int*)&imageHeader[28];
 
-		imageData = (unsigned char*)malloc (imgDataSize * sizeof(unsigned char));
-		newimageData = (unsigned char*)malloc (imgDataSize * sizeof(unsigned char));
+	int imgDataSize = width * height; 
 
-		if(bitDepth <= 8)	
-			fread(colorTable, sizeof(unsigned char), 1024, fp);
-			
+	imageData = (unsigned char*)malloc (imgDataSize * sizeof(unsigned char));
+	newimageData = (unsigned char*)malloc (imgDataSize * sizeof(unsigned char));
 
-		fread(imageData, sizeof(unsigned char), imgDataSize, fp);
-				
-		for(i = 0; i < height; i++){
-					for(j = 0; j < width; j++){                   
-						newimageData[i*width + j] = 255 - imageData[i*width + j]; 
-				}   
-		}
+	if(bitDepth <= 8)	
+		fread(colorTable, sizeof(unsigned char), 1024, fp);
 
-		char* newFilename = "negation_res.bmp";
-		FILE *fo = fopen(newFilename, "wb");
 
-		fwrite(imageHeader, sizeof(unsigned char), 54, fo);
+	fread(imageData, sizeof(unsigned char), imgDataSize, fp);
 
-		if(bitDepth <= 8)
-						fwrite(colorTable, sizeof(unsigned char), 1024, fo);
+	for(i = 0; i < height; i++){
+		for(j = 0; j < width; j++){                   
+			newimageData[i*width + j] = 255 - imageData[i*width + j]; 
+		}   
+	}
 
-		fwrite( newimageData, sizeof(unsigned char), imgDataSize, fo); 
+	char* newFilename = "negation_res.bmp";
+	FILE *fo = fopen(newFilename, "wb");
 
-		fclose(fo);
-		fclose(fp);
+	fwrite(imageHeader, sizeof(unsigned char), 54, fo);
+
+	if(bitDepth <= 8)
+		fwrite(colorTable, sizeof(unsigned char), 1024, fo);
+
+	fwrite(newimageData, sizeof(unsigned char), imgDataSize, fo); 
+
+	fclose(fo);
+	fclose(fp);
 }
 
 int main()
 {
 	clock_t start, stop; 
 	for(int i = 1; i <= 50; i++){
-			start=clock();
+		start=clock();
 		
-			negation("testLenna.bmp");
+		negation("testLenna.bmp");
 
-			stop = clock(); 
-			double d = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;                                                   
-			printf("%lf\n", i, d);
+		stop = clock(); 
+		double d = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;                                                   
+		printf("%lf\n", i, d);
 	}	
 }
